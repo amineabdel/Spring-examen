6a7409d498a36a96921a176f2f8f6abdd03f18e5
amineabdel <amine.oostende@hotmail.com>
2019-06-21 11:26:38 +0200

    search recipe erbij gedaan

--- Spring-examen/src/main/java/edu/ap/spring/tml/controller/TMLController.java
+++ Spring-examen/src/main/java/edu/ap/spring/tml/controller/TMLController.java
@@ -26,32 +26,47 @@ public class TMLController {
    @PostMapping("new")
    public String postRecipe(@RequestParam("name") String name, @RequestParam("ingredients") String ingredients,
          Model m) {
-      /*
-       * if (!service.exists("*recipe*")) { service.setKey("recipe", name + "|" +
-       * ingredients); System.out.println("if");
-       * 
-       * } else { service.getKey("recipe"); System.out.println("else"); }
-       */
+
+      if (!service.exists("*recipe*")) {
+         service.setKey("recipe", name + "|" + ingredients);
+         System.out.println("if");
+
+      } else {
+         service.getKey("recipe");
+         System.out.println("else");
+      }
 
       service.setKey("recipe", name + "|" + ingredients);
       service.getKey("recipe");
 
-      List<String> recipeList = new ArrayList<String>();
-      Set<String> names = service.keys("recipe:*");
-      for (String n : names) {
-         String rec = "";
-         Map<Object, Object> recipe = service.hgetAll(n);
-         String[] parts = n.split(" ");
-         System.out.println(parts);
-      }
+      /*
+       * List<String> recipeList = new ArrayList<String>(); Set<String> names =
+       * service.keys("recipe:*"); for (String n : names) { String rec = "";
+       * Map<Object, Object> recipe = service.hgetAll(n); String[] parts =
+       * n.split(" "); System.out.println(parts); }
+       */
+
+      // ik kan als ik een recept aanmaak, dat het zichtbaar is in de console
       System.out.println(service.getKey("recipe"));
       return "newRecipe";
 
    }
 
-   @GetMapping("/search")
-   public String getSearchForm() {
-
+   @GetMapping("/searchRecipe")
+   public String getSearch() {
       return "searchRecipe";
    }
+
+   @GetMapping("/searchRecipe/{name}")
+   public String getSearchForm(@PathVariable("name") String name, Model m) {
+      m.addAttribute("recipe", service.getKey("recipe"));
+      return "redirect:/searchRecipe";
+   }
+
+   @PostMapping("/search")
+   public String postSearch(@RequestParam("name") String name, Model m) {
+      service.setKey("recipe", name);
+      return "redirect:/searchRecipe/" + name;
+   }
+
 }

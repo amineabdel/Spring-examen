11e2605d0bd6d86f3f6c44f177449b0ef00abb63
amineabdel <amine.oostende@hotmail.com>
2019-06-21 09:14:08 +0200

    keys + pagina tonen

--- Spring-examen/src/main/java/edu/ap/spring/tml/controller/TMLController.java
+++ Spring-examen/src/main/java/edu/ap/spring/tml/controller/TMLController.java
@@ -1,7 +1,13 @@
 package edu.ap.spring.tml.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import edu.ap.spring.redis.RedisService;
@@ -14,13 +20,38 @@ public class TMLController {
 
    @GetMapping("/new")
    public String getCreateForm() {
+      return "newRecipe";
+   }
 
-	   return "newRecipe";
+   @PostMapping("new")
+   public String postRecipe(@RequestParam("name") String name, @RequestParam("ingredients") String ingredients,
+         Model m) {
+      /*
+       * if (!service.exists("*recipe*")) { service.setKey("recipe", name + "|" +
+       * ingredients); System.out.println("if");
+       * 
+       * } else { service.getKey("recipe"); System.out.println("else"); }
+       */
+
+      service.setKey("recipe", name + "|" + ingredients);
+      service.getKey("recipe");
+
+      List<String> recipeList = new ArrayList<String>();
+      Set<String> names = service.keys("recipe:*");
+      for (String n : names) {
+         String rec = "";
+         Map<Object, Object> recipe = service.hgetAll(n);
+         String[] parts = n.split(" ");
+         System.out.println(parts);
+      }
+      System.out.println(service.getKey("recipe"));
+      return "newRecipe";
+
    }
 
    @GetMapping("/search")
    public String getSearchForm() {
 
-	   return "searchRecipe";
+      return "searchRecipe";
    }
 }
